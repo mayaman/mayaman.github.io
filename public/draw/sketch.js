7eb9478f295143d923cba8e34da5f426e78b569f
mayaman26 <mayaman26@gmail.com>
2015-12-30 22:47:34 -0500

    fixed p5 library to support sketches

--- public/draw/sketch.js
+++ public/draw/sketch.js
@@ -13,13 +13,13 @@ function setup() {
 
 
 function draw() {
-  if (x > 0) {
+  //if (x > 0) {
   strokeWeight(4);
   line(pmouseX, pmouseY, mouseX, mouseY);
   line(pmouseX, pmouseY + 10, mouseX, mouseY+10);
   line(pmouseX, pmouseY + 20, mouseX, mouseY+20);
   line(pmouseX, pmouseY + 30, mouseX, mouseY+30);
-  }
+  //}
 }
 
 function mouseClicked() {
