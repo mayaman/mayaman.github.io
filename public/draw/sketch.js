fe49122f57000ee2d95265f5b166b77a8c83ce5d
mayaman26 <mayaman26@gmail.com>
2016-02-17 18:15:56 -0800

    fixed spelling error

--- public/draw/sketch.js
+++ public/draw/sketch.js
@@ -16,13 +16,13 @@ function windowResized() {
 }
 
 function draw() {
-  //if (x > 0) {
+  if (x > 0) {
   strokeWeight(4);
   line(pmouseX, pmouseY, mouseX, mouseY);
   line(pmouseX, pmouseY + 10, mouseX, mouseY+10);
   line(pmouseX, pmouseY + 20, mouseX, mouseY+20);
   line(pmouseX, pmouseY + 30, mouseX, mouseY+30);
-  //}
+  }
 }
 
 function mouseClicked() {
