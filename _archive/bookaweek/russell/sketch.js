2798039c99aca0f505ee56ed8920656a8d032e78
Maya Man <mayaman@google.com>
2019-07-06 02:50:13 -0400

    style and layout update, bio update, added talks and workshops

---
+++ _archive/bookaweek/russell/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  var canvas = createCanvas(500, 500);
+}
+
+
+function draw() {
+  background(255);
+}
+
+
+
+function windowResized() {
+  // resizeCanvas(windowWidth, windowHeight);
+  // width = windowWidth;
+  // height = windowHeight;
+}
